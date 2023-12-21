631df8205ec72ac0527904ecaa4b9e5e6c34ee3a
Andre Juan <andrjetx@yahoo.com.br>
2023-12-21 13:05:45 -0300

    Feat: finalizaçao light mode e ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -41,4 +41,25 @@ document.addEventListener("DOMContentLoaded", function () {
   
   toggle.addEventListener("change", () => {
     document.body.classList.toggle("light-mode");
+    const images = document.querySelectorAll('.imagem-home img');
+    const lightModePaths = [
+      "./src/img/eat-home-lm.png",
+      "./src/img/sleep-home-lm.png",
+      "./src/img/code-home-lm.png",
+      "./src/img/repeat-home-lm.png"
+    ];
+
+    const darkModePaths = [
+      "./src/img/eat-home-dm.png",
+      "./src/img/sleep-home-dm.png",
+      "./src/img/code-home-dm.png",
+      "./src/img/repeat-home-dm.png"
+    ];
+
+    const isLightMode = document.body.classList.contains("light-mode");
+
+    images.forEach((image, index) => {
+      image.src = isLightMode ? lightModePaths[index] : darkModePaths[index];
+    });
+
   });
